629df9f35347eee9e3891b8e4ca9b6356b067ddf
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-12-21 05:24:51 +0200

    test

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let countriesContainer = document.querySelector('.countries');
-
 var chosen_countryName = $('input').val();
 
 function setup() {
@@ -14,20 +12,5 @@ function getCountry(countryName) {
 
   xhr.addEventListener('load', function(){
     let [data] = JSON.parse(xhr.responseText);
-
-    let html = '
-      <article class="country">
-        <img class="country_image" src="${data.flag}" />
-        <div class="country_data">
-          <h3 class="country_name">${data.name}</h3>
-          <h4 class="country_region">${data.region}</h4>
-          <p class="country_row"><span class="ion-ios-people"</span>${data.population}</p>
-            <p class="country_row"><span class="ion-ios-mic"</span>${data.languages[0].name}</p>
-          <p class="country_row"><span class="ion-social-usd"</span>${data.currencies[0].name}</p>
-        </div>
-      </artcile>
-    ';
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
   });
 }
