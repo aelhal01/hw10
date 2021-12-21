865405670a41b5d3bca63e629ede330f153e621a
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-12-21 05:57:55 +0200

    big change

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//gets user input chosen country
 var chosen_countryName = $('input').val();
 
+//start searching for country info once submit button is pressed
 function setup() {
   var button = select('#submit');
   button.mousePressed(getCountry(chosen_countryName));
 }
 
+//search and return selected country info
 function getCountry(countryName) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://restcountries.com/v3.1/name/'+countryName, true);
@@ -12,5 +15,30 @@ function getCountry(countryName) {
 
   xhr.addEventListener('load', function(){
     let [data] = JSON.parse(xhr.responseText);
+
+    //Replace country image source in HTML file
+    let countryImage = document.getElementById("country_image").src;
+    countryImage.innerHTML = ${data.flag};
+
+    //Replace country name in HTML file
+    let countryname = document.getElementById("country_name");
+    countryname.innerHTML = ${data.name};
+
+
+    //Replace country region in HTML file
+    let countryRegion = document.getElementById("country_region");
+    countryRegion.innerHTML = ${data.region};
+
+    //Replace country population in HTML file
+    let countryPopulation = document.getElementById("ion-ios-people");
+    countryPopulation.innerHTML = ${data.population};
+
+    //Replace country language in HTML file
+    let countryLanguage = document.getElementById("ion-ios-mic");
+    countryLanguage.innerHTML = ${data.languages[0].name};
+
+    //Replace country currency in HTML file
+    let countryCurrency = document.getElementById("ion-social-usd");
+    countryCurrency.innerHTML = ${data.currencies[0].name};
   });
 }
