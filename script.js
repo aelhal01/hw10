d35e75593637d85fd3bb550ec5b7a99a18bf5322
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-12-21 04:49:12 +0200

    test

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let countriesContainer = document.querySelector(.countries);
 
-var countryName = $('input').val();
+var chosen_countryName = $('input').val();
 
 function setup() {
   var button = select('#submit');
-  button.mousePressed(getCountry);
+  button.mousePressed(getCountry(chosen_countryName));
 }
 
-function getCountry() {
+function getCountry(countryName) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://restcountries.com/v3.1/name/'+countryName, true);
   xhr.send();
