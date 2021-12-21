852789227c9a122d6cb6c7238617f297f0bd78b9
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-12-21 03:47:46 +0200

    updates

---
+++ script.js
@@ -0,0 +1,31 @@
+let countriesContainer = document.querySelector(.countries);
+function getCountry(countryName) {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://restcountries.com/v3.1/name/'+countryName, true);
+
+  xhr.send();
+
+  xhr.addEventListener('load', function(){
+    let [data] = JSON.parse(xhr.responseText);
+
+    let html = '
+      <article class="country">
+        <img class="country_image" src="${data.flag}" />
+        <div class="country_data">
+          <h3 class="country_name">${data.name}</h3>
+          <h4 class="country_region">${data.region}</h4>
+          <p class="country_row"><span class="ion-ios-people"</span>${data.population}</p>
+            <p class="country_row"><span class="ion-ios-mic"</span>${data.languages[0].name}</p>
+          <p class="country_row"><span class="ion-social-usd"</span>${data.currencies[0].name}</p>
+        </div>
+      </artcile>
+    ';
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+  });
+}
+
+getCountry('usa');
+getCountry('brazil');
+getCountry('germany');
